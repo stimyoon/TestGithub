2ea315d628df71377a77dd3d566fc6b936429fad
stimyoon <64033604+stimyoon@users.noreply.github.com>
2022-01-28 13:47:30 -0500

    Initial Commit

---
+++ TestGithub/TestGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubApp.swift
+//  TestGithub
+//
+//  Created by Tim Yoon on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
